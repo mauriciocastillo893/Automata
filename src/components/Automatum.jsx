1bcb8b5fe1f1f1da398428ef7a3be6cc14c70d61
CassDeLaRy <mauriciomtz893@gmail.com>
2023-11-13 21:10:39 -0600

    09 10pm

--- src/components/Automatum.jsx
+++ src/components/Automatum.jsx
@@ -1,5 +1,9 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import CodeMirror from "@uiw/react-codemirror";
 import '../style-sheets/Automatum.css'
+import {
+    validateVariableDeclaration
+  } from "./grammar"; 
 
 function Automatum() {
     const [inputResult, setInputResult] = useState("")
@@ -50,59 +54,10 @@ function Automatum() {
       :
     `
 
-    const grammar = {
-        "EX": /^string NVS;\s*$/,
-        "NVS": /^[a-zA-Z_][a-zA-Z0-9_]* = ".+"$/,
-    };
-
-    const grammarExpression = [
-        {key: "EX", terminal: "string", next_key: "NVS"},
-        {key: "NVS", terminal: ["a", "b"], next_key: "NVSS"},
-        {key: "NVSS", terminal: "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z", next_key: "NVS"},
-    ]
-
-    function contarPalabras(texto) {
-        var palabras = texto.split(/[^\S\n]+/);
-        palabras = palabras.filter(function(palabra) {
-            return palabra.length > 0;
-        });
-        return palabras.length;
-    }
-    
-    function contarEspacios(cadena) {
-        var espacios = cadena.split(" ").length - 1;
-        return espacios;
-    }
-
-    function detectExpression(cadena) {
-        console.log("cantidad de espacios", contarEspacios(cadena))
-    
-        if (cadena && contarEspacios(cadena) <= 0) {
-            let found = false;
-    
-            grammarExpression.forEach((expression) => {
-                // console.log(expression.terminal)
-                if (expression.terminal === cadena) {
-                    console.log("entre")
-                    console.log("matched", expression.terminal === cadena)
-                    setInputResult(expression.terminal);
-                    found = true;
-                }
-            });
-    
-            if (!found) {
-                console.log("not found");
-                setInputResult("not found");
-            }
-        } else {
-            console.log("no hay cadena");
-        }
-    }
-    
-
-    const handleSearchExpression = (e) => {
-        detectExpression(e.target.value)
-    }
+    const onChange = useCallback((value) => {
+        const result = validateVariableDeclaration(value);
+        setInputResult(result);
+    }, []);
 
     return (
         <div className='main-container'>
@@ -115,14 +70,19 @@ function Automatum() {
                     <textarea className='description' value={documentation} readOnly/>
                 </div>
                 <div className="second-content">
-                    <div className="second-one-content">
-                        <div className='title-s-o-c'></div>
-                        <textarea className="code" onChange={(e) => { handleSearchExpression(e) }} />
-                    </div>
-                    <div className="second-two-content">
-                        <div className='title-s-t-c'></div>
-                        <textarea className="terminal" value={inputResult} readOnly />
-                    </div>
+                <CodeMirror
+            value=""
+            height="400px"
+            width="600px"
+            theme="dark"
+            onChange={onChange}
+            className="py-2"
+          />
+          {inputResult && (
+        <div style={{color: "#fff"}}>
+          {inputResult}
+        </div>
+      )}
                 </div>
             </div>
         </div>
---
+++ src/components/grammar.js
@@ -0,0 +1,531 @@
+const stateMap = {
+    "qe": [
+        {
+            nextState: "q010",
+            error: "Palabra reservada mal escrita",
+            rule: /^int$/,
+        },
+        {
+            nextState: "q020",
+            error: "Palabra reservada mal escrita",
+            rule: /^float$/,
+        },
+        {
+            nextState: "q030",
+            error: "Palabra reservada mal escrita",
+            rule: /^bool$/,
+        },
+        {
+            nextState: "q040",
+            error: "Palabra reservada mal escrita",
+            rule: /^string$/,
+        },
+        {
+            nextState: "q050",
+            error: "Palabra reservada mal escrita",
+            rule: /^char$/,
+        },
+
+        {
+            nextState: "qfr",
+            error: "Nombre de variable no válido o palabra reservada mal escrita",
+            rule: /^([a-z][a-z0-9_]*)(.read)$/,
+        },
+        {
+            nextState: "qfw",
+            error: "Nombre de variable no válido o palabra reservada mal escrita",
+            rule: /^(\(".*?"\)\.write$|\([a-z][a-z0-9_]*\)\.write)$/,
+        },
+        {
+            nextState: "q20",
+            error: "Nombre de variable no válido o palabra reservada mal escrita",
+            rule: /^\(".*$/
+        },
+        {
+            nextState: "q2",
+            error: "Palabra reservada mal escrita",
+            rule: /^fnc$/,
+        },
+        {
+            nextState: "q6",
+            error: "Palabra reservada mal escrita",
+            rule: /^switch$/,
+        },
+        {
+            nextState: "q12",
+            error: "Palabra reservada mal escrita",
+            rule: /^while$/,
+        },
+
+    ],
+    "q010": [
+        {
+            nextState: "q011",
+            error: "Nombre de variable no válido",
+            rule: /^[a-z][a-zA-Z0-9_]*$/,
+        },
+    ],
+    "q011": [
+        {
+            nextState: "q013",
+            error: "Coma detectada",
+            rule: /^;$/,
+        },
+        {
+            nextState: "q012",
+            error: "Operador no válido",
+            rule: /^=$/,
+        },
+        {
+            nextState: "q010",
+            error: "Separacion de variables no válido",
+            rule: /^,$/,
+        }
+    ],
+    "q012": [
+        {
+            nextState: "q013",
+            error: "Valor no válido",
+            rule: /^[0-9]*$/,
+        },
+    ],
+    "q013": [
+        {
+            nextState: "q010",
+            error: "Separacion de variables no válido",
+            rule: /^,$/,
+        }
+    ],
+    "q020": [
+        {
+            nextState: "q021",
+            error: "Nombre de variable no válido",
+            rule: /^[a-z][a-z0-9_]*$/,
+        },
+    ],
+    "q021": [
+        {
+            nextState: "q022",
+            error: "Operador no válido",
+            rule: /^=$/,
+        },
+        {
+            nextState: "q020",
+            error: "Separacion de variables no válido",
+            rule: /^,$/,
+        }
+    ],
+    "q022": [
+        {
+            nextState: "q023",
+            error: "Valor no válido",
+            rule: /^[0-9]*.[0-9]*$/,
+        },
+    ],
+    "q023": [
+        {
+            nextState: "q020",
+            error: "Separacion de variables no válido",
+            rule: /^,$/,
+        }
+    ],
+    "q030": [
+        {
+            nextState: "q031",
+            error: "Nombre de variable no válido",
+            rule: /^[a-z][a-z0-9_]*$/,
+        },
+    ],
+    "q031": [
+        {
+            nextState: "q032",
+            error: "Operador no válido",
+            rule: /^=$/,
+        },
+        {
+            nextState: "q030",
+            error: "Separacion de variables no válido",
+            rule: /^,$/,
+        }
+    ],
+    "q032": [
+        {
+            nextState: "q033",
+            error: "Valor no válido",
+            rule: /^(true|false)*$/,
+        },
+    ],
+    "q033": [
+        {
+            nextState: "q030",
+            error: "Separacion de variables no válido",
+            rule: /^,$/,
+        }
+    ],
+    "q040": [
+        {
+            nextState: "q0401",
+            error: "Nombre de variable no válido",
+            rule: /^[a-z][a-z0-9]*$/,
+        },
+    ],
+    "q0401": [
+        {
+            nextState: "q041",
+            error: "Semicolon expected",
+            rule: /^\s;$/,
+        },
+    ],
+    "q041": [
+        {
+            nextState: "q042",
+            error: "Operador no válido",
+            rule: /^=$/,
+        },
+        {
+            nextState: "q040",
+            error: "Separacion de variables no válido",
+            rule: /^,$/,
+        }
+    ],
+    "q042": [
+        {
+            nextState: "q043",
+            error: "Valor no válido",
+            rule: /^".*"$/
+        },
+        {
+            nextState: "q044",
+            error: "Valor no válido",
+            rule: /^".*$/
+        },
+    ],
+    "q043": [
+        {
+            nextState: "q040",
+            error: "Separacion de variables no válido",
+            rule: /^,$/,
+        }
+    ],
+    "q044": [
+        {
+            nextState: "q043",
+            error: "Valor no válido o hace falta comillas",
+            rule: /^[^"]*"$/
+        },
+        {
+            nextState: "q044",
+            error: "Palabra no válido",
+            rule: /^[^"]+$/
+        }
+    ],
+    "q1": [
+        {
+            nextState: "qfd",
+            error: "Nombre de variable no válido",
+            rule: /^[a-z][a-z0-9_]*$/,
+        },
+    ],
+    "q2": [
+        {
+            nextState: "q3",
+            error: "Nombre de variable no válido",
+            rule: /^[a-z][a-z0-9_]*$/,
+        }
+    ],
+    "q3": [
+        {
+            nextState: "q4",
+            error: "Cierre y apertura no válido",
+            rule: /^(\(\){$|^\([a-z][a-z0-9_]*\){)$/
+        },
+        {
+            nextState: "q003",
+            error: "Cierre y apertura no válido",
+            rule: /^\([a-z][a-z0-9_]*$/
+        }
+    ],
+    "q003": [
+        {
+            nextState: "q004",
+            error: "Separacion de variables no válido",
+            rule: /^,$/,
+        }
+    ],
+    "q004": [
+        {
+            nextState: "q4",
+            error: "Cierre y apertura no válido",
+            rule: /^[a-z][a-z0-9_]*\){$/
+        },
+        {
+            nextState: "q003",
+            error: "Cierre y apertura no válido",
+            rule: /^[a-z][a-z0-9_]*$/
+
+        }
+    ],
+    "q4": [
+        {
+            nextState: "q5",
+            error: "Nombre de variable no válido o palabra reservada mal escrita",
+            rule: /^(\(".*?"\)\.write$|\([a-z][a-z0-9_]*\)\.write|([a-z][a-z0-9_]*)(.read))$/,
+        },
+        {
+            nextState: "q050",
+            error: "Nombre de variable no válido o inicio mal escrito",
+            rule: /^\(".*$/
+        },
+    ],
+    "q050": [
+        {
+            nextState: "q5",
+            error: "Palabra no válida o cierre incorrecto",
+            rule: /^[^"]*"\)\.write$/
+        },
+        {
+            nextState: "q050",
+            error: "Palabra no válido",
+            rule: /^[^"]+$/
+        }
+    ],
+    "q5": [
+        {
+            nextState: "qff",
+            error: "Cierre no válido",
+            rule: /^\}$/,
+        },
+    ],
+    "q6": [
+        {
+            nextState: "q7",
+            error: "Nombre de variable de switch no válido",
+            rule: /^\([a-z][a-z0-9_]*\){$/,
+        },
+    ],
+    "q7": [
+        {
+            nextState: "q8",
+            error: "Case no válido",
+            rule: /^case_[a-z][a-z0-9_]*\($/,
+        },
+    ],
+    "q8": [
+        {
+            nextState: "q9",
+            error: "Introducción no válido",
+            rule: /^(\(".*?"\)\.write$|\([a-z][a-z0-9_]*\)\.write|([a-z][a-z0-9_]*)(.read))$/,
+        },
+        {
+            nextState: "q090",
+            error: "Nombre de variable no válido o inicio mal escrito",
+            rule: /^\(".*$/
+        },
+    ],
+    "q090": [
+        {
+            nextState: "q9",
+            error: "Palabra no válida o cierre incorrecto",
+            rule: /^[^"]*"\)\.write$/
+        },
+        {
+            nextState: "q090",
+            error: "Palabra no válido",
+            rule: /^[^"]+$/
+        }
+    ],
+    "q9": [
+        {
+            nextState: "q10",
+            error: "Cierre no válido",
+            rule: /\)$/,
+        },
+    ],
+    "q10": [
+        {
+            nextState: "q8",
+            error: "Case no válido",
+            rule: /^case_[a-z][a-z0-9_]*\($/,
+        },
+        {
+            nextState: "q11",
+            error: "Palabra reservada mal escrita",
+            rule: /^exit$/,
+        },
+    ],
+    "q11": [
+        {
+            nextState: "qfs",
+            error: "Cierre no válido",
+            rule: /}$/,
+        },
+    ],
+    "q12": [
+        {
+            nextState: "q13",
+            error: "Nombre de variable de while no válido",
+            rule: /^\([a-z][a-z0-9_]*$/,
+        },
+    ],
+    "q13": [
+        {
+            nextState: "q14",
+            error: "Operador no válido",
+            rule: /^==$/,
+        },
+    ],
+    "q14": [
+        {
+            nextState: "q15",
+            error: "Valor no válido",
+            rule: /^(true\)|false\){)$/
+        },
+    ],
+    "q15": [
+        {
+            nextState: "q16",
+            error: "Introducción no válido",
+            rule: /^(\(".*?"\)\.write$|\([a-z][a-z0-9_]*\)\.write|([a-z][a-z0-9_]*)(.read))$/,
+        },
+        {
+            nextState: "q016",
+            error: "Nombre de variable no válido o inicio mal escrito",
+            rule: /^\(".*$/
+        },
+
+    ],
+    "q016": [
+        {
+            nextState: "q16",
+            error: "Palabra no válida o cierre incorrecto",
+            rule: /^[^"]*"\)\.write$/
+        },
+        {
+            nextState: "q016",
+            error: "Palabra no válido",
+            rule: /^[^"]+$/
+        }
+    ],
+    "q16": [
+        {
+            nextState: "q11",
+            error: "Palabra reservada no válido",
+            rule: /^exit$/,
+        },
+    ],
+    "q17": [
+        {
+            nextState: "q18",
+            error: "Operador no válido",
+            rule: /^=$/,
+        }
+    ],
+    "q18": [
+        {
+            nextState: "q19",
+            error: "Valor no válido",
+            rule: /^([0-9]*|[0-9]*.[0-9]*|true|false|".*")$/,
+        },
+        {
+            nextState: "q19",
+            error: "Valor no válido",
+            rule: /^".*$/
+        },
+    ],
+    "q19": [
+        {
+            nextState: "q20",
+            error: "Valor no válido o hace falta comillas",
+            rule: /^[^"]*"$/
+        },
+        {
+            nextState: "q19",
+            error: "Palabra no válido",
+            rule: /^[^"]+$/
+        }
+    ],
+    "q20": [
+        {
+            nextState: "qfw",
+            error: "Palabra no válida o cierre incorrecto",
+            rule: /^[^"]*"\)\.write$/
+        },
+        {
+            nextState: "q20",
+            error: "Palabra no válido",
+            rule: /^[^"]+$/
+        }
+    ],
+};
+
+export function validateVariableDeclaration(value) {
+    let currentState = "qe";
+    const lines = value.split('\n');
+
+    for (const line of lines) {
+        const tokens = line.trim().split(' ');
+        for (const token of tokens) {
+            const nextState = getNextState(currentState, token);
+            console.log(nextState)
+            if (nextState === "q0-error") {
+                console.log("error1")
+                return `Error: ${currentState}: ${stateMap[currentState][0].error}`;
+            }
+            if ((currentState === "q1" && token.endsWith('_')) || (currentState === "q010" && token.endsWith('_')) || (currentState === "q020" && token.endsWith('_')) || (currentState === "q030" && token.endsWith('_')) || (currentState === "q0401" && token.endsWith('_')) || (currentState === "qe" && token.endsWith('_'))) {
+                console.log("error2")
+                return `Error: Nombre de variable no puede terminar en '_'`;
+            }
+            currentState = nextState;
+        }
+    }
+
+    if (currentState === "qfd" || currentState === "q011" || currentState === "q021" || currentState === "q031" || currentState === "q041") {
+        return "Declaración de variable válida";
+    }
+    if (currentState === "q013" || currentState === "q023" || currentState === "q033" || currentState === "q043") {
+        return "Declaración e inicializacion de variable válido";
+    }
+    if (currentState === "q19" || currentState === "q20") {
+        return "Inicialización de variable válido";
+    }
+    if (currentState === "qfr") {
+        return "Leer variable válido";
+    }
+    if (currentState === "qfw") {
+        return "Escribir variable válido";
+    }
+    if (currentState === "qff") {
+        return "Declaración de función válido";
+    }
+    if (currentState === "qfs") {
+        return "Declaración de funcion de sentencia válido";
+    }
+    else {
+        return `Error: ${currentState}: ${stateMap[currentState][0].error}`;
+    }
+}
+
+function getNextState(currentState, token) {
+    const stateTransitions = stateMap[currentState];
+    if (stateTransitions) {
+        for (const transition of stateTransitions) {
+            if (transition.rule.test(token)) {
+                return transition.nextState;
+            }
+        }
+    }
+    return "q0-error";
+}
+
+/*const readline = require('readline');
+ 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+ 
+rl.question('Ingrese el bloque de codigo: ', (input) => {
+  const result = validateVariableDeclaration(input);
+  console.log(result);
+  rl.close();
+});*/
